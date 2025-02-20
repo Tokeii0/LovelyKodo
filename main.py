3ff754e866ff27ef05452aa9aaf342403cb3f8da
猫捉鱼 <111427585+Tokeii0@users.noreply.github.com>
2025-02-20 21:36:52 +0800

    优化 字体相关 适配 PySide6-6.8.2.1

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
+from style import dynamic_styles
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    
+    # 应用全局样式
+    app.setStyleSheet(dynamic_styles())
+    
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
